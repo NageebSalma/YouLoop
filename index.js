f220de79d5c9165c6337598eeb6ea9b9f910b7cf
NageebSalma <sleem.eaas@gmail.com>
2022-08-05 20:05:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+// var popup = require('popups')
+var app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function(req, res) {
+  res.render('youloop',{videoID:"H5v3kku4y6Q" , start : 0 , end : 60});
+});
+app.post("/",function(req,res){
+   
+    // console.log(req.body.url)
+    let url = req.body.url;
+    if(!url || !req.body.start || !req.body.end) res.send("please fill all fields.")
+    let videoID = ""
+    var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+var match = url.match(regExp);
+if (match && match[2].length == 11) {
+  videoID = match[2];
+} 
+    let startMins = +req.body.start.split(":")[0]
+    let startSeconds = +req.body.start.split(":")[1]
+    let endMins = +req.body.end.split(":")[0]
+    let endSeconds = +req.body.start.split(":")[1]
+    let start = startMins*60+startSeconds;
+    // console.log(start)
+    let end = endMins*60+endSeconds;
+    res.render('youloop',{videoID: videoID , start : start , end : end});
+
+})
+
+
+app.listen( process.env.PORT|| 3000,function(){
+    console.log('Server is listening on port 3000');
+}
+);
